8c4f3243195219ebe1dbe09105a505b9cc355c42
Lucas Borges <lucasbfernandes94@gmail.com>
2021-11-22 02:23:44 -0300

    Removing unnecessary script parts

--- test/e2e/utilities/counter/response_time.py
+++ test/e2e/utilities/counter/response_time.py
@@ -3,24 +3,18 @@ import numpy as np
 from time import sleep
 
 NUMBER_OF_COMMANDS = 10000
-TEST_REPETITIONS = 1
-SERVICE_URL = "http://localhost/counter/integer"
+TEST_REPETITIONS = 10
+SERVICE_URL = "http://stress-service:3000/integer"
 
 def fire_and_get_response_times(url):
     response_times = []
     for i in range(NUMBER_OF_COMMANDS):
         response = requests.get(url)
         response_times.append(response.elapsed.total_seconds() * 1000)
-#         sleep(0.05)
+
     return response_times
 
 def main():
-    means = []
-    std_deviations = []
-    percentiles_90 = []
-    percentiles_95 = []
-    percentiles_99 = []
-
     for i in range(TEST_REPETITIONS):
         print("Executing test instance number: ", i + 1)
 
@@ -33,23 +27,11 @@ def main():
         percentile_95 = np.percentile(response_array, 95)
         percentile_99 = np.percentile(response_array, 99)
 
-        means.append(mean)
-        std_deviations.append(std_deviations)
-        percentiles_90.append(percentile_90)
-        percentiles_95.append(percentile_95)
-        percentiles_99.append(percentile_99)
-
         print("Mean: ", str(mean).replace(".", ","))
         print("Standard deviation: ", str(std).replace(".", ","))
         print("90th percentile: ", str(percentile_90).replace(".", ","))
         print("95th percentile: ", str(percentile_95).replace(".", ","))
         print("99th percentile: ", str(percentile_99).replace(".", ","))
         print("")
-
-    print("Final mean: ", np.mean(np.array(means)))
-    print("Final standard deviation: ", np.mean(np.array(std_deviations)))
-    print("Final 90th percentile: ", np.mean(np.array(percentiles_90)))
-    print("Final 95th percentile: ", np.mean(np.array(percentiles_95)))
-    print("Final 99th percentile: ", np.mean(np.array(percentiles_99)))
 
 main()

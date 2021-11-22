e24d489506c7e489b631563170916eeb97fa79d0
Lucas Borges <lucasbfernandes94@gmail.com>
2021-11-22 00:29:51 -0300

    Update tests and image tags

--- test/e2e/utilities/counter/response_time.py
+++ test/e2e/utilities/counter/response_time.py
@@ -1,7 +1,9 @@
 import requests
 import numpy as np
+from time import sleep
 
 NUMBER_OF_COMMANDS = 10000
+TEST_REPETITIONS = 1
 SERVICE_URL = "http://localhost/counter/integer"
 
 def fire_and_get_response_times(url):
@@ -9,22 +11,45 @@ def fire_and_get_response_times(url):
     for i in range(NUMBER_OF_COMMANDS):
         response = requests.get(url)
         response_times.append(response.elapsed.total_seconds() * 1000)
+#         sleep(0.05)
     return response_times
 
 def main():
-    response_times = fire_and_get_response_times(SERVICE_URL)
-    response_array = np.array(response_times)
+    means = []
+    std_deviations = []
+    percentiles_90 = []
+    percentiles_95 = []
+    percentiles_99 = []
 
-    mean = np.mean(response_array)
-    std = np.std(response_array)
-    percentile_90 = np.percentile(response_array, 90)
-    percentile_95 = np.percentile(response_array, 95)
-    percentile_99 = np.percentile(response_array, 99)
+    for i in range(TEST_REPETITIONS):
+        print("Executing test instance number: ", i + 1)
 
-    print("Mean: ", mean)
-    print("Standard deviation: ", std)
-    print("90th percentile: ", percentile_90)
-    print("95th percentile: ", percentile_95)
-    print("99th percentile: ", percentile_99)
+        response_times = fire_and_get_response_times(SERVICE_URL)
+        response_array = np.array(response_times)
+
+        mean = np.mean(response_array)
+        std = np.std(response_array)
+        percentile_90 = np.percentile(response_array, 90)
+        percentile_95 = np.percentile(response_array, 95)
+        percentile_99 = np.percentile(response_array, 99)
+
+        means.append(mean)
+        std_deviations.append(std_deviations)
+        percentiles_90.append(percentile_90)
+        percentiles_95.append(percentile_95)
+        percentiles_99.append(percentile_99)
+
+        print("Mean: ", str(mean).replace(".", ","))
+        print("Standard deviation: ", str(std).replace(".", ","))
+        print("90th percentile: ", str(percentile_90).replace(".", ","))
+        print("95th percentile: ", str(percentile_95).replace(".", ","))
+        print("99th percentile: ", str(percentile_99).replace(".", ","))
+        print("")
+
+    print("Final mean: ", np.mean(np.array(means)))
+    print("Final standard deviation: ", np.mean(np.array(std_deviations)))
+    print("Final 90th percentile: ", np.mean(np.array(percentiles_90)))
+    print("Final 95th percentile: ", np.mean(np.array(percentiles_95)))
+    print("Final 99th percentile: ", np.mean(np.array(percentiles_99)))
 
 main()
